856f4fc647c7be68a28b6348f412ea86105e77b8
darkelectron <darkelectron@mailfence.com>
2024-01-11 02:35:37 +0200

    focus: add autocommands to ignore filetypes

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -69,3 +69,35 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
     end
   end,
 })
+
+-- focus addon
+local ignore_filetypes = { 'alpha', 'NvimTree', 'help', 'Outline' }
+local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+
+local augroup =
+    vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+
+vim.api.nvim_create_autocmd('WinEnter', {
+    group = augroup,
+    callback = function(_)
+        if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
+        then
+            vim.w.focus_disable = true
+        else
+            vim.w.focus_disable = false
+        end
+    end,
+    desc = 'Disable focus autoresize for BufType',
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    group = augroup,
+    callback = function(_)
+        if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+            vim.b.focus_disable = true
+        else
+            vim.b.focus_disable = false
+        end
+    end,
+    desc = 'Disable focus autoresize for FileType',
+})
