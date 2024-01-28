8a526b854a1fb33fdf17ada66d8116d44f069322
darkelectron <darkelectron@mailfence.com>
2024-01-28 12:26:21 +0200

    changes in alpha

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -4,7 +4,7 @@ local M = {
 }
 
 function M.config()
-  local alpha = require "alpha"
+  -- local alpha = require "alpha"
   local dashboard = require "alpha.themes.dashboard"
   dashboard.section.header.val = {
     [[    ⢰⣧⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⠄⠄⠄⡀⠄⢀⣿⣿⠄⠄⠄⢸⡇⠄⠄ ]] ,
@@ -27,9 +27,10 @@ function M.config()
     dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
     dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
     dashboard.button("p", " " .. " Find project", ":lua require('telescope').extensions.projects.projects()<CR>"),
-    dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+    dashboard.button("r", "󰊄 " .. " Recent files", ":Telescope oldfiles <CR>"),
     dashboard.button("t", " " .. " Find text", ":Telescope live_grep <CR>"),
     dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+    dashboard.button("u", "  Update plugins", "<cmd>Lazy sync<CR>"),
     dashboard.button("q", " " .. " Quit", ":qa<CR>"),
   }
   local function footer()
@@ -43,7 +44,7 @@ function M.config()
   dashboard.section.buttons.opts.hl = "Keyword"
 
   dashboard.opts.opts.noautocmd = true
-  alpha.setup(dashboard.opts)
+  require("alpha").setup(dashboard.opts)
 end
 
 return M
