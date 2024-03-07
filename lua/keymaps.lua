c1cb01a95d2745f70820d26220535b038caa8627
darkelectron <darkelectron@mailfence.com>
2024-03-07 23:32:11 +0200

    make keymaps work for flutter-tools

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -100,13 +100,17 @@ keymap("n", "<leader>pv", ":vsp<cr>", opts)
 keymap("n", "<leader>ps", ":sp<cr>", opts)
 
 -- flutter-tools
---  " Show hover
--- nnoremap K <Cmd>lua vim.lsp.buf.hover()<CR>
---  " Jump to definition
--- nnoremap gd <Cmd>lua vim.lsp.buf.definition()<CR>
---  " Open code actions using the default lsp UI, if you want to change this please see the plugins above
--- nnoremap <leader>ca <Cmd>lua vim.lsp.buf.code_action()<CR>
---  " Open code actions for the selected visual range
--- xnoremap <leader>ca <Cmd>lua vim.lsp.buf.range_code_action()<CR>
-keymap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
 keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+keymap("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+keymap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+keymap("n", "<leader>li", "<cmd>LspInfo<cr>", opts)
+keymap("n", "<leader>M", "<cmd>Mason<cr>", opts)
+keymap("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+keymap("n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts)
+keymap("n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
+keymap("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+keymap("n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+keymap("n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
