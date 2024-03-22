43738df7a5f705c9b77c68972251d680176982f0
darkelectron <darkelectron@mailfence.com>
2024-03-23 01:21:06 +0200

    remove flake8 in none

--- lua/user/none-ls.lua
+++ lua/user/none-ls.lua
@@ -14,7 +14,7 @@ function M.config()
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
   local formatting = null_ls.builtins.formatting
   -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-  local diagnostics = null_ls.builtins.diagnostics
+  -- local diagnostics = null_ls.builtins.diagnostics
 
   -- https://github.com/prettier-solidity/prettier-plugin-solidity
   null_ls.setup {
@@ -27,7 +27,11 @@ function M.config()
       formatting.black.with { extra_args = { "--fast" } },
       formatting.stylua,
       formatting.google_java_format,
-      diagnostics.flake8.with { extra_args = { "--max-line-length", "120" }},
+      formatting.dart_format,
+      -- diagnostics.flake8.with { extra_args = { "--max-line-length", "120" }},
+      -- null_ls.builtins.diagnostics.flake8,
+      -- null_ls.builtins.completion.spell,
+      -- null_ls.builtins.completion.luasnip,
     },
   }
 end
