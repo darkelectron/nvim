b6f13d03c751e83a0dd3e4c73d1cc5031663f066
darkelectron <darkelectron@mailfence.com>
2024-04-02 04:26:50 +0200

    change vimwiki notes folder

--- lua/user/vimwiki.lua
+++ lua/user/vimwiki.lua
@@ -2,6 +2,6 @@ return {
    'vimwiki/vimwiki',
   event = "VimEnter",
    init = function () --replace 'config' with 'init'
-      vim.g.vimwiki_list = {{path = '~/Documents/vimwiki/', syntax = 'markdown', ext = '.md'}}
+      vim.g.vimwiki_list = {{path = '~/Documents/vimwiki_notes/', syntax = 'markdown', ext = '.md'}}
    end
 }
