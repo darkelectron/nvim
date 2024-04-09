89f767b5871f5a2c8020aae38dcaec969f893f19
darkelectron <darkelectron@mailfence.com>
2024-04-09 07:30:03 +0200

    load vimwiki on keybinding

--- lua/user/vimwiki.lua
+++ lua/user/vimwiki.lua
@@ -1,7 +1,14 @@
 return {
-   'vimwiki/vimwiki',
-  event = "VimEnter",
-   init = function () --replace 'config' with 'init'
-      vim.g.vimwiki_list = {{path = '~/Documents/vimwiki_notes/', syntax = 'markdown', ext = '.md'}}
-   end
+  'vimwiki/vimwiki',
+  keys = {
+    {
+      "<leader>ww",
+      "<cmd>VimwikiIndex<cr>",
+      desc = "load vimwiki"
+    }
+  },
+  init = function () --replace 'config' with 'init'
+    vim.g.vimwiki_list = {{path = '~/Documents/vimwiki_notes/', syntax = 'markdown', ext = '.md'}}
+    vim.g.vimwiki_tag_format = {conceal = 1}
+  end
 }
