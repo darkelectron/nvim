b62e19935d458f82988b1ce913b78147d2ca690b
darkelectron <darkelectron@mailfence.com>
2024-04-09 07:58:34 +0200

    add catppuccin theme

--- init.lua
+++ init.lua
@@ -5,4 +5,5 @@ require "autocommands"
 require "plugconfigs.slanted-gaps"
 vim.cmd([[source ~/.config/nvim/basic-settings.vim]])
 vim.cmd 'colorscheme tokyonight-moon'
+vim.cmd 'colorscheme catppuccin'
 require "vimwiki_preamble"
